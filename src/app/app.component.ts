9277e0ec3a1684156cf20372504928281c8ae5ef
ParulPetal <parulsingh550@gmail.com>
2019-07-11 11:21:12 +0530

    Weather search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'weather';
+  
+  country: string = '';
+  city: string = '';
+  info: any;
+  API_KEY = "e1fa1058d81ff910c3bef4a951a0c038";
+  httpdata: any;
+  temp:any; 
+  temp_max:any;
+  temp_min:any;
+  pressure:any;
+  humidity:any;
+  Show:boolean=true;
+  main:object;
+  
+ constructor(private httpClient: HttpClient) { }
+
+  onCountryKeyUp(event: any) {
+    this.country = event.target.value;
+
+  }
+  onCityKeyUp(event: any) {
+    this.city = event.target.value;
+ }
+getTemperature() {
+
+    this.httpClient.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&APPID=${this.API_KEY}&units=metric`)
+      .subscribe((data) => { this.displaydata(data); }
+      )
+  }
+  displaydata(data) {
+  this.httpdata = data;
+
+//console.log(this.httpdata);
+  }
+
 }
